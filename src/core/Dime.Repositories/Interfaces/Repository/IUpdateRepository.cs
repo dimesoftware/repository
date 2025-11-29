be9cf6cfee1f3f23ecfa822d23b91d60836fde0e
Hendrik Bulens <hendrik.bulens@gmail.com>
2025-11-29 19:02:39 +0100

    Use ExecuteUpdate

--- src/core/Dime.Repositories/Interfaces/Repository/IUpdateRepository.cs
+++ src/core/Dime.Repositories/Interfaces/Repository/IUpdateRepository.cs
@@ -57,5 +57,58 @@ namespace Dime.Repositories
         /// <param name="properties">The properties of the entity to update</param>
         /// <returns>The updated entity</returns>
         Task<TEntity> UpdateAsync(TEntity entity, params string[] properties);
+
+        /// <summary>
+        /// Updates entities matching the where clause by setting a property to a new value.
+        /// Uses ExecuteUpdate for efficient bulk updates without loading entities.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the property value</typeparam>
+        /// <param name="where">Expression to filter entities to update</param>
+        /// <param name="propertySelector">Expression selecting the property to update</param>
+        /// <param name="newValue">The new value to set</param>
+        /// <returns>The number of entities updated</returns>
+        Task<int> UpdateAsync<TValue>(Expression<Func<TEntity, bool>> where, Expression<Func<TEntity, TValue>> propertySelector, TValue newValue);
+
+        /// <summary>
+        /// Updates entities matching the where clause by setting a property using an expression that references the existing value.
+        /// Uses ExecuteUpdate for efficient bulk updates without loading entities.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the property value</typeparam>
+        /// <param name="where">Expression to filter entities to update</param>
+        /// <param name="propertySelector">Expression selecting the property to update</param>
+        /// <param name="valueExpression">Expression that calculates the new value based on the existing entity</param>
+        /// <returns>The number of entities updated</returns>
+        Task<int> UpdateAsync<TValue>(
+            Expression<Func<TEntity, bool>> where,
+            Expression<Func<TEntity, TValue>> propertySelector,
+            Expression<Func<TEntity, TValue>> valueExpression);
+
+        /// <summary>
+        /// Updates entities matching the where clause (synchronous version).
+        /// Uses ExecuteUpdate for efficient bulk updates without loading entities.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the property value</typeparam>
+        /// <param name="where">Expression to filter entities to update</param>
+        /// <param name="propertySelector">Expression selecting the property to update</param>
+        /// <param name="newValue">The new value to set</param>
+        /// <returns>The number of entities updated</returns>
+        int Update<TValue>(
+            Expression<Func<TEntity, bool>> where,
+            Expression<Func<TEntity, TValue>> propertySelector,
+            TValue newValue);
+
+        /// <summary>
+        /// Updates entities matching the where clause by setting a property using an expression that references the existing value (synchronous version).
+        /// Uses ExecuteUpdate for efficient bulk updates without loading entities.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the property value</typeparam>
+        /// <param name="where">Expression to filter entities to update</param>
+        /// <param name="propertySelector">Expression selecting the property to update</param>
+        /// <param name="valueExpression">Expression that calculates the new value based on the existing entity</param>
+        /// <returns>The number of entities updated</returns>
+        int Update<TValue>(
+            Expression<Func<TEntity, bool>> where,
+            Expression<Func<TEntity, TValue>> propertySelector,
+            Expression<Func<TEntity, TValue>> valueExpression);
     }
 }
--- src/test/Dime.Repositories.Sql.EntityFramework.Tests/UpdateTests.cs
+++ src/test/Dime.Repositories.Sql.EntityFramework.Tests/UpdateTests.cs
@@ -47,5 +47,81 @@ namespace Dime.Repositories.Sql.EntityFramework.Tests
             Blog blog = context.Blogs.Find(1);
             Assert.IsTrue(blog.Url == "http://sample.com/cats");
         }
+
+        [TestMethod]
+        public void Update_WithExecuteUpdate_ConstantValue_ShouldUpdateMatchingEntities()
+        {
+            using TestDatabase testDb = new();
+
+            using IRepository<Blog> repo = new EfRepository<Blog, BloggingContext>(new BloggingContext(testDb.Options));
+            
+            // Update blogs with BlogId 1 and 2 (the ones with "cat" in URL)
+            int updated = repo.Update(
+                b => b.BlogId == 1 || b.BlogId == 2,
+                b => b.Url,
+                "http://sample.com/updated");
+
+            // Verify the number of entities updated
+            Assert.AreEqual(2, updated);
+
+            // Use a separate instance of the context to verify correct data was saved to database
+            using BloggingContext context = new(testDb.Options);
+            Blog blog1 = context.Blogs.Find(1);
+            Blog blog2 = context.Blogs.Find(2);
+            Blog blog3 = context.Blogs.Find(3);
+
+            Assert.AreEqual("http://sample.com/updated", blog1.Url);
+            Assert.AreEqual("http://sample.com/updated", blog2.Url);
+            Assert.AreEqual("http://sample.com/dogs", blog3.Url); // Should not be updated
+        }
+
+        [TestMethod]
+        public void Update_WithExecuteUpdate_ExpressionValue_ShouldUpdateMatchingEntities()
+        {
+            using TestDatabase testDb = new();
+
+            using IRepository<Blog> repo = new EfRepository<Blog, BloggingContext>(new BloggingContext(testDb.Options));
+            
+            // Update all blogs to have a modified URL using an expression
+            int updated = repo.Update(
+                b => b.BlogId <= 2,
+                b => b.Url,
+                b => b.Url + "/modified");
+
+            // Verify the number of entities updated
+            Assert.AreEqual(2, updated);
+
+            // Use a separate instance of the context to verify correct data was saved to database
+            using BloggingContext context = new(testDb.Options);
+            Blog blog1 = context.Blogs.Find(1);
+            Blog blog2 = context.Blogs.Find(2);
+            Blog blog3 = context.Blogs.Find(3);
+
+            Assert.IsTrue(blog1.Url.EndsWith("/modified"));
+            Assert.IsTrue(blog2.Url.EndsWith("/modified"));
+            Assert.AreEqual("http://sample.com/dogs", blog3.Url); // Should not be updated
+        }
+
+        [TestMethod]
+        public void Update_WithExecuteUpdate_NoMatchingEntities_ShouldReturnZero()
+        {
+            using TestDatabase testDb = new();
+
+            using IRepository<Blog> repo = new EfRepository<Blog, BloggingContext>(new BloggingContext(testDb.Options));
+            
+            // Try to update blogs that don't exist
+            int updated = repo.Update(
+                b => b.BlogId > 100,
+                b => b.Url,
+                "http://sample.com/updated");
+
+            // Verify no entities were updated
+            Assert.AreEqual(0, updated);
+
+            // Verify no data was changed
+            using BloggingContext context = new(testDb.Options);
+            Blog blog1 = context.Blogs.Find(1);
+            Assert.AreEqual("http://sample.com/cats", blog1.Url);
+        }
     }
 }
